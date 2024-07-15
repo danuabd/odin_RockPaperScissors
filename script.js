b685a5e3a3a164178da7a57b7d30e2ee76c346c7
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2024-07-15 20:48:52 +0530

    Create getComputerChoice to return computer choice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const getComputerChoice = function () {
+  const randomNum = Math.trunc(Math.random() * 3 + 1);
+
+  if (randomNum === 1) return "Rock";
+  if (randomNum === 2) return "Paper";
+  if (randomNum === 3) return "Scissors";
+};
+
+getComputerChoice();
